e7ed24cc69c8e8428e4f688d5a21adb6bab23186
abuwcheek <abdulloistamov1034@gmail.com>
2024-11-30 10:39:35 +0500

    complete ToDoList

---
+++ mainApp/serializers.py
@@ -0,0 +1,10 @@
+from rest_framework import serializers
+from .models import *
+
+
+
+
+class ToDoSerializer(serializers.ModelSerializer):
+     class Meta:
+          model = ToDo
+          fields = ('id', 'title', 'description', 'date', 'completed',)
